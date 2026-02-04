bf8ef50d0587d58402a6e6547f2f3dc2be2a1c8e
Pedrohenriquezuza <phzuza07@gmail.com>
2026-02-04 19:25:24 -0300

    fix: calendário não abria ao clicar no input

--- src/components/inputs/CalendarInput.tsx
+++ src/components/inputs/CalendarInput.tsx
@@ -1,15 +1,16 @@
 import CalendarIcon from "../../assets/icons/CalendarBlank.svg?react";
 import CaretDown from "../../assets/icons/CaretDown.svg?react";
+
 type Props = {
   value: string;
   onChange: (date: string) => void;
 };
+
 export default function CalendarInput({ value, onChange }: Props) {
   return (
     <div className="relative mt-2">
       <CalendarIcon
-        className="absolute top-2 left-3
-       w-5"
+        className="absolute top-2 left-3 w-5 pointer-events-none"
         fill="#b8952e"
       />
 
@@ -19,15 +20,27 @@ export default function CalendarInput({ value, onChange }: Props) {
         value={value}
         onChange={(e) => onChange(e.target.value)}
         max="2099-12-31"
-        className="h-12 mr-3
-    bg-transparent
-    w-full
-    [&::-webkit-calendar-picker-indicator]:opacity-0 border-2 border-gray-500 rounded-lg pl-10 relative outline-0 focus:border-2 focus:border-yellow-dark
-  "
+        className="
+          h-12
+          w-full
+          bg-transparent
+          border-2 border-gray-500
+          rounded-lg
+          pl-10 pr-10
+          outline-0
+          focus:border-yellow-dark
+
+          [&::-webkit-calendar-picker-indicator]:absolute
+          [&::-webkit-calendar-picker-indicator]:inset-0
+          [&::-webkit-calendar-picker-indicator]:w-full
+          [&::-webkit-calendar-picker-indicator]:h-full
+          [&::-webkit-calendar-picker-indicator]:opacity-0
+          [&::-webkit-calendar-picker-indicator]:cursor-pointer
+        "
       />
+
       <CaretDown
-        className="absolute top-2 right-3
-       w-5 "
+        className="absolute top-2 right-3 w-5 pointer-events-none"
         fill="#98959d"
       />
     </div>
